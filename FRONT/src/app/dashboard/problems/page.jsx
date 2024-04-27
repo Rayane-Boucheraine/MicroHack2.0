556e0d0610f534f04f523d7b55fe5a779fb0f40e
Rayane-Boucheraine <rayaneha08@gmail.com>
2024-04-27 05:21:35 +0100

    add responsive

---
+++ FRONT/src/app/dashboard/problems/page.jsx
@@ -0,0 +1,13 @@
+import Problems from '@/components/dashboard/problems/Problems'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col items-center'>
+      <Problems />
+      <Problems />
+    </div>
+  )
+}
+
+export default page
