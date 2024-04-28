2f6215d88977eb9c7d045a209c4558d3868eb354
Rayane-Boucheraine <rayaneha08@gmail.com>
2024-04-29 00:36:43 +0100

    last commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,6 +10,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <title>BuildFlow</title>
+      </head>
       <body className={inter.className}>
         <QueryClientProvider client={queryClient}>
           {children}{" "}
